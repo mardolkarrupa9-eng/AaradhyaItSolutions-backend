574a6aba0f4b3fd10b245a64fceeb0f6a340383f
britniefernandes07-netizen <britniefernandes07@gmail.com>
2026-06-04 16:46:23 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
+    'http://localhost:5174',  // ← add this
     'https://rhearods.github.io'
   ],
   credentials: true
