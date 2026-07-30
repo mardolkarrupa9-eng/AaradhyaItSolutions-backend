92bc8bac3a22a9c98286cb026179e1fec105d2bf
Rhea Rodrigues <rhearodrigues106@gmail.com>
2026-07-31 01:33:09 +0530

    Add PATCH method to CORS

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use(cors({
     'https://aaradhyaitsolutions.netlify.app',
     'http://localhost:5173'
   ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   credentials: true
 
 
